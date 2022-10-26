ff161a3b4842867d8777dbb1dc5a42529a56053e
Aymeric-Olivaux <aymeric.olivaux@epita.fr>
2022-10-26 20:36:47 +0200

    [ADD][EXK] Brute

--- LevelK/Brute.php
+++ LevelK/Brute.php
@@ -14,14 +14,37 @@ class Brute
     }
 
     /**
-     * @TODO :
      *
      * Cette méthode essaye de trouver par la force à quel mot de 4 lettres correspond ce hash.
      * Sachant que nous ne connaissons pas le hash (enfin si... il suffit de regarder les commentaires de l'attribut privé $methode.
      */
     public function force()
     {
+    for ($i=65; $i<=90; $i++) { 
+	for ($j=65; $j<=90; $j++) {     
+	    for ($k=65; $k<=90; $k++) {     
+		for ($t=65; $t<=90; $t++) {     
+		    $var = chr($i).chr($j).chr($k).chr($t)."<br>";      
+		    $md5 = md5($var);
+		    $crc32 = crc32($var);
+		    $base64 = base64_encore($var);
+		    $sha1 = sha1($var);
 
-        // @TODO
+		    if ($hash === $md5) {
+			$origin = $var;
+		    }
+		    if ($hash === $crc32) {
+			$origin = $var;
+		    }
+ 		    if ($hash === $base64) {
+			$origin = $var;
+		    }
+		    if ($hash === $sha1) {
+			$origin = $var;
+		    }
+		    print($var);
+}
+}
+}
     }
 }
