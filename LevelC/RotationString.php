6bebd9e96fb36c3508b5b673f084c8af6481d858
Milann Guitton <milann.guitton@epita.fr>
2022-10-26 19:53:30 +0200

    [ADD][PHP] LevelC

--- LevelC/RotationString.php
+++ LevelC/RotationString.php
@@ -14,9 +14,17 @@ class RotationString
      */
     public static function isRotation($s1, $s2)
     {
-        /** @TODO */
-
-        return false;
+        $len = strlen($s1);
+        $res = false;
+        $n = $len;
+        while ($n > 0 && !$res) {
+            $s1 = substr($s1, -1) . substr($s1, 0, $len - 1);
+            if ($s1 == $s2) {
+                $res = true;
+            }
+            $n--;
+        }
+        return $res;
     }
 
     public static function isSubString($s1, $s2)
